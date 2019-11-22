5b7865e147b56ec8d65ec696da2a3d5776d59e63
derrickkarake <47511082+derrickkarake@users.noreply.github.com>
2019-11-22 12:10:55 -0600

    Update treesum.c

--- treesum.c
+++ treesum.c
@@ -1,64 +1,148 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
 #include <time.h>
 #include <sys/time.h>
+#include <sys/msg.h>
+#include <sys/queue.h>
 
-// Returns a random value between -1 and 1
-double getRand(unsigned int *seed) {
-    return (double) rand_r(seed) * 2 / (double) (RAND_MAX) - 1;
+static const long Num_To_Add = 1000000000;
+static const double Scale = 10.0 / RAND_MAX;
+
+struct Queue {
+    omp_lock_t lock;
+    LIST_HEAD(message_list, Message) messages;
+};
+
+struct Message {
+    long value;
+    LIST_ENTRY(Message) pointers;
+};
+
+// Sends a message to the given queue;
+void send_msg(struct Queue *queue, long message) {
+    omp_set_lock(&queue->lock);
+    struct Message *mess = malloc(sizeof(struct Message));
+    mess->value = message;
+    LIST_INSERT_HEAD(&queue->messages, mess, pointers);
+    omp_unset_lock(&queue->lock);
 }
 
-long double Calculate_Pi_Sequential(long long number_of_tosses) {
-    long long number_in_circle = 0;
-    unsigned int seed = (unsigned int) time(NULL);
-    for (long long int toss = 0; toss < number_of_tosses; toss++) {
-        double x = getRand(&seed);
-        double y = getRand(&seed);
-        double distance_squared = x*x + y*y;
-        if (distance_squared <= 1) number_in_circle++;
+// Attempts to get the newest message in the queue, if there is no message, -1 is returned.
+long try_recv(struct Queue *queue) {
+    long result = -1;
+    omp_set_lock(&queue->lock);
+    if (!LIST_EMPTY(&queue->messages)) {
+        struct Message *head = LIST_FIRST(&queue->messages);
+        result = head->value;
+        LIST_REMOVE(head, pointers);
     }
-    return 4 * number_in_circle/((double) number_of_tosses);
+    omp_unset_lock(&queue->lock);
+    return result;
 }
 
-long double Calculate_Pi_Parallel(long long number_of_tosses) {
-    long long number_in_circle = 0;
-#pragma omp parallel num_threads(20)
-{
-    // Since rand() is not thread-safe, we generate a seed per thread and use rand_r() to retrieve a
-    // thread-safe random value.
-    unsigned int seed = (unsigned int) time(NULL) + (unsigned int) omp_get_thread_num();
-    #pragma omp for reduction(+: number_in_circle)
-        for (long long int toss = 0; toss < number_of_tosses; toss++) {
-            double x = getRand(&seed);
-            double y = getRand(&seed);
-            double distance_squared = x*x + y*y;
-            if (distance_squared <= 1) number_in_circle++;
+// Blocks until a message is received.
+long block_recv(struct Queue *queue) {
+    long result = -1;
+    while (result == -1) {
+        result = try_recv(queue);
+    }
+    return result;
+}
+
+long add_serial(const char *numbers) {
+    long sum = 0;
+    for (long i = 0; i < Num_To_Add; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+long add_parallel(const char *numbers) {
+    long sum = 0;
+    int thread_count = omp_get_max_threads();
+
+    // Setup queues for each thread
+    struct Queue *queues = malloc(thread_count * sizeof(struct Queue));
+    for (int i = 0; i < thread_count; i++) {
+        struct Queue queue;
+        omp_init_lock(&queue.lock);
+        LIST_INIT(&queue.messages);
+        queues[i] = queue;
+    }
+
+#pragma omp parallel num_threads(thread_count)
+    {
+        long my_sum = 0;
+        int my_rank = omp_get_thread_num();
+
+        // Each thread sums up their own portion of the array
+#pragma omp for
+        for (int i = 0; i < Num_To_Add; i++) {
+            my_sum += numbers[i];
         }
+
+        // Use a tree sum to find total sum of the elements in the array.
+        int divisor = 2;
+        int rank_difference = 1;
+
+        while(1) {
+            if (my_rank % divisor == 0) {
+                // This thread is a receiver on this round.
+                int sender = my_rank + rank_difference;
+                if (sender >= thread_count)
+                    break;
+                long value = block_recv(&queues[my_rank]);
+                my_sum += value;
+            } else {
+                // This thread is a sender on this round.
+                int recipient = my_rank - rank_difference;
+                if (recipient < 0)
+                    break;
+                send_msg(&queues[recipient], my_sum);
+            }
+            divisor *= 2;
+            rank_difference *= 2;
+        }
+
+        // The final sum is located in the thread with rank 0
+        if (my_rank == 0)
+            sum = my_sum;
     }
-    return 4 * number_in_circle/((double) number_of_tosses);
+
+    free(queues);
+
+    return sum;
 }
 
 int main() {
-    struct timeval start, end;
+    // Due to limitations with how rand() works, we cannot parallelize this generation
+    char *numbers = malloc(sizeof(long) * Num_To_Add);
+    for (long i = 0; i < Num_To_Add; i++) {
+        numbers[i] = (char) (rand() * Scale);
+    }
 
-    long long num_tosses = 10000000;
+    struct timeval start, end;
 
     printf("Timing sequential...\n");
     gettimeofday(&start, NULL);
-    long double sequential_pi = Calculate_Pi_Sequential(num_tosses);
+    long sum_s = add_serial(numbers);
     gettimeofday(&end, NULL);
-    printf("Took %f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
+    double time_s = end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000;
+    printf("Took %f seconds\n\n", time_s);
 
     printf("Timing parallel...\n");
     gettimeofday(&start, NULL);
-    long double parallel_pi = Calculate_Pi_Parallel(num_tosses);
+    long sum_p = add_parallel(numbers);
     gettimeofday(&end, NULL);
-    printf("Took %f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
+    double time_p = end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000;
+    printf("Took %f seconds\n\n", time_p);
 
-    // This will print the result to 10 decimal places
-    printf("π = %.10Lf (sequential)\n", sequential_pi);
-    printf("π = %.10Lf (parallel)\n", parallel_pi);
+    printf("Sum serial: %ld\nSum parallel: %ld\n", sum_s, sum_p);
 
+    printf("Speedup factor of %.4f\n", time_s/time_p);
+
+    free(numbers);
     return 0;
 }
